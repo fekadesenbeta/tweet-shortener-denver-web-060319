f0c9d6de6db31d67f9fd61ccc3f33c8013db66f1
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-28 17:25:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def dictionary
+  hash = {
+  "hello"=> "hi",
+  "to, two, too" => "2",
+  "for, four" => "4",
+  'be' => "b",
+  'you' => "u",
+  "at" => "@" ,
+  "and" => "&"
+  }
+end
+
+def word_substituter(tweet_one)
+  array = []
+  tweet_one.each do |words|
+    words.split.downcase 
+  end
+  
+end
