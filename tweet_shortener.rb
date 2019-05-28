e86bf038fc9d2c11e70398ade0dcabedd82445d2
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-28 20:19:19 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ end
 def word_substituter(tweet)
   #tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, #you know what I mean? Tweeting is super fun you guys!!!!" 
  
-  new_array = tweet.split(" ")map do |word|
+  new_array = tweet.split(" ").map do |word|
     if dictionary.keys.include?(word)
       word = dictionary[value]
     else
