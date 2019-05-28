159d0e112e7e236ec0d7b19b714445d6baa181cd
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-28 21:31:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,14 +14,14 @@ def dictionary
 end
 
 def word_substituter (tweet)
-  words_to_be_shortened = tweet.split.collect do |word|
+  tweet.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
       word
     end
   end
-  words_to_be_shortened.join(" ")
+  tweet.join(" ")
 end
 
 def bulk_tweet_shortener(array)
