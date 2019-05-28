3d0d2ae1697e854e14a4bfb799ea061d4ece9edf
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-28 19:58:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,16 +25,6 @@ end
 end
 
 
-def word_substituter(tweet) # takes a tweet string and makes substitutions
-  tweet.split(' ').collect do |word| # for each word in the tweet array
-    if dictionary.keys.include?(word.downcase) #if the array of dict keys includes a tweet word
-      word = dictionary[word.downcase] # replace the word with the associated value
-    else
-      word # otherwise return the original word so it doesn't show 'nil'
-    end
-  end.join(' ')
-end
-
 
 
 
