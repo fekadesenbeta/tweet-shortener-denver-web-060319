4dbb82b765c4e8be36963461faed7786cbdb7782
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-28 18:19:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -10,14 +10,14 @@ def dictionary
   }
 end
 
-def word_substituter(tweet)
-  tweet.split(" ").collect do |word|
-   if dictionary.keys.include?(word.downcase)
-     word = dictionary[word.downcase]
-   end
-   tweet.join(" ")
- end
- 
+def word_substituter(tweet) # takes a tweet string and makes substitutions
+  tweet.split(' ').collect do |word| # for each word in the tweet array
+    if dictionary.keys.include?(word.downcase) # if the array of dict keys includes a downcase tweet word
+      word = dictionary[word.downcase] # replace the word with the associated value
+    else
+      word # otherwise return the original word so it doesn't show 'nil'
+    end
+  end.join(' ')
 end
 
 
